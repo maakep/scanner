1fd1fef245fe4cf7d906b12320239fd11bfb091b
Maakep <schon.fredrik@gmail.com>
2020-03-11 20:14:02 +0100

    Initial commit

---
+++ src/components/App/App.test.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
+
+test("nothing", () => {
+  expect(1).toEqual(1);
+});
---
+++ src/components/App/App.tsx
@@ -0,0 +1,8 @@
+import React, { useState } from 'react';
+
+function App() {
+
+  return <div></div>;
+}
+
+export default App;
